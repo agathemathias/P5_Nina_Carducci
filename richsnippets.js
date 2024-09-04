74703013a82721c0d1e263ccd241c13b8709f6fb
Agathe Mathias <agathe.mathias8@gmail.com>
2024-09-04 14:47:37 +0200

    Il ne reste que le problème de barre de navigation à faire.

---
+++ richsnippets.js
@@ -0,0 +1,28 @@
+{
+    "@context"= "https://schema.org",
+    "@type"= "LocalBusiness",
+    "name"= "Nina Carducci",
+    "image"= "",
+    "@id"= "",
+    "url"= "",
+    "telephone"= "05 56 67 78 89",
+    "address"= {
+      "@type": "PostalAddress",
+      "streetAddress": "68 avenue Alsace-Lorraine",
+      "addressLocality": "Bordeaux",
+      "postalCode": "33200",
+      "addressCountry": "FR"
+    },
+    "openingHoursSpecification"= {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      "opens": "10:00",
+      "closes": "19:00"
+    } 
+}

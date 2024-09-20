a20bfe4b11e182eb4be09adf6aebb36678e156e0
Agathe Mathias <agathe.mathias8@gmail.com>
2024-09-20 18:15:46 +0200

    Copier coller des anciens bootstrap + changt lightboxId

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             xl: 3
         },
         lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
+        lightboxId: 'galleryLightbox',
         showTags: true,
         tagsPosition: 'top'
     });
